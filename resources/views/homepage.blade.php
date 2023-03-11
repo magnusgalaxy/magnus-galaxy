b71cd2661659e2f67c9d1c82c56c2f85cf830159
Sahil Chahal <dev.sahilchahal@gmail.com>
2023-03-11 22:13:08 +0530

    Initial home page done

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.app')
 @section('content')
-    Home Page
+<div class="border-b border-grey-lighter py-16 lg:py-20">
+    <h1 class="pt-3 font-body text-4xl font-semibold text-primary dark:text-white md:text-3xl lg:text-3xl">Welcome to the {{ config('app.name', 'Magnus Galaxy') }}</h1>
+    <p class="pt-3 font-body text-xl font-light text-primary dark:text-white">
+        Stay up-to-date with the latest trends and best practices in technology, and take your skills to the next level with our informative and engaging content.
+    </p>
+    <a href="/"class="mt-12 block bg-secondary px-10 py-4 text-center font-body text-xl font-semibold text-white transition-colors hover:bg-green sm:inline-block sm:text-left sm:text-2xl">
+      Say Hello!
+    </a>
+</div>
 @endsection
