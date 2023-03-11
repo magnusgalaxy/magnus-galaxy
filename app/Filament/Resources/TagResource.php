e7075cf9b5176dde9ddf07d6e2a982a9ae8100ca
Sahil Chahal <dev.sahilchahal@gmail.com>
2023-03-11 23:43:49 +0530

    Tag Resource done

---
+++ app/Filament/Resources/TagResource.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TagResource\Pages;
+use App\Filament\Resources\TagResource\RelationManagers;
+use App\Models\Tag;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TagResource extends Resource
+{
+    protected static ?string $model = Tag::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-hashtag';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Fieldset::make('Tag Details')
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->required()
+                            ->label('Tag Name'),
+                        Forms\Components\Textarea::make('description'),
+                    ]),
+                Forms\Components\Fieldset::make('SEO Details')
+                    ->schema([
+                        Forms\Components\Textarea::make('meta_title'),
+                        Forms\Components\Textarea::make('meta_description'),
+                        Forms\Components\Textarea::make('meta_keywords'),
+                    ])
+                    ->columns(3),
+
+                Forms\Components\Toggle::make('is_active'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('slug')
+                    ->searchable(),
+                Tables\Columns\BooleanColumn::make('is_active')
+                    ->label('Active'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTags::route('/'),
+            'create' => Pages\CreateTag::route('/create'),
+            'edit' => Pages\EditTag::route('/{record}/edit'),
+        ];
+    }    
+}
