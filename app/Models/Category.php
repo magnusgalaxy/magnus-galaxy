3a9b89d53b8467ba9839bf28eb9a676e589dd752
Sahil Chahal <dev.sahilchahal@gmail.com>
2023-03-11 22:53:45 +0530

    Sluggable Implemented

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
 
     /**
      * The attributes that are mass assignable.
@@ -25,4 +27,14 @@ class Category extends Model
         'meta_keywords',
         'is_active'
     ];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 }
