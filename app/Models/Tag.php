9be7b5c99491e88add5daeb5b333b38b0a818d1d
Sahil Chahal <dev.sahilchahal@gmail.com>
2023-03-11 23:43:36 +0530

    Sluggable implemented on Tags

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
 
     /**
      * The attributes that are mass assignable.
@@ -23,4 +25,15 @@ class Tag extends Model
         'meta_keywords',
         'is_active'
     ];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug')
+            ->doNotGenerateSlugsOnUpdate();
+    }
 }
