02cd390e320dfe67ba15d31a56b1fa6dd884dd34
Sahil Chahal <dev.sahilchahal@gmail.com>
2023-04-09 20:58:19 +0530

    SEO resumed

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -13,12 +13,12 @@ class BlogController extends Controller
 {
     public function index(){
         //SEO Tags
-        // SEOTools::setTitle('Blogs');
-        // SEOTools::setDescription("Explore the latest posts and updates on PHP, Laravel, Reactjs, Angular, and other programming languages on Magnus Galaxy's blogs page. Stay updated with our insightful articles and tutorials.");
-        // SEOTools::opengraph()->setUrl(url()->current());
-        // SEOTools::opengraph()->addProperty('type', 'articles');
-        // SEOTools::twitter()->setSite('@magnusgalaxy');
-        // SEOTools::jsonLd()->addImage(asset('assets/site-images/logo.svg'));
+        SEOTools::setTitle('Blogs');
+        SEOTools::setDescription("Explore the latest posts and updates on PHP, Laravel, Reactjs, Angular, and other programming languages on Magnus Galaxy's blogs page. Stay updated with our insightful articles and tutorials.");
+        SEOTools::opengraph()->setUrl(url()->current());
+        SEOTools::opengraph()->addProperty('type', 'articles');
+        SEOTools::twitter()->setSite('@magnusgalaxy');
+        SEOTools::jsonLd()->addImage(asset('assets/site-images/logo.svg'));
         
 
         $blogs = Blog::with('categories')->paginate(20);
@@ -29,27 +29,27 @@ class BlogController extends Controller
     public function show(Blog $blog){
         $categories = $blog->categories;
  
-        // SEOMeta::setTitle($blog->meta_title ?? $blog->title);
-        // SEOMeta::setDescription($blog->meta_description);
-        // SEOMeta::addMeta('article:published_time', $blog->created_at, 'property');
-        // foreach($categories as $category){
-        //     SEOMeta::addMeta('article:section', $category->name, 'property');
-        // }
-        // SEOMeta::addKeyword([$blog->keywords]);
+        SEOMeta::setTitle($blog->meta_title ?? $blog->title);
+        SEOMeta::setDescription($blog->meta_description);
+        SEOMeta::addMeta('article:published_time', $blog->created_at, 'property');
+        foreach($categories as $category){
+            SEOMeta::addMeta('article:section', $category->name, 'property');
+        }
+        SEOMeta::addKeyword([$blog->keywords]);
 
-        // OpenGraph::setDescription($blog->meta_description);
-        // OpenGraph::setTitle($blog->meta_title ?? $blog->title);
-        // OpenGraph::setUrl(url()->current());
-        // OpenGraph::addProperty('type', 'article');
-        // OpenGraph::addProperty('locale', 'pt-br');
-        // OpenGraph::addProperty('locale:alternate', ['pt-pt', 'en-us']);
+        OpenGraph::setDescription($blog->meta_description);
+        OpenGraph::setTitle($blog->meta_title ?? $blog->title);
+        OpenGraph::setUrl(url()->current());
+        OpenGraph::addProperty('type', 'article');
+        OpenGraph::addProperty('locale', 'pt-br');
+        OpenGraph::addProperty('locale:alternate', ['pt-pt', 'en-us']);
 
-        // OpenGraph::addImage(asset('assets/site-images/logo.svg'));
+        OpenGraph::addImage(asset('assets/site-images/logo.svg'));
 
-        // JsonLd::setTitle($blog->meta_title ?? $blog->title);
-        // JsonLd::setDescription($blog->meta_description);
-        // JsonLd::setType('Article');
-        // JsonLd::addImage(asset('assets/site-images/logo.svg'));
+        JsonLd::setTitle($blog->meta_title ?? $blog->title);
+        JsonLd::setDescription($blog->meta_description);
+        JsonLd::setType('Article');
+        JsonLd::addImage(asset('assets/site-images/logo.svg'));
 
         return view('blogs.show', compact('blog'));
     }
